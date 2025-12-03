9ecb283f0e6002d4f689bab9e9a18b5427fa17c0
Sewanou Romaric Aholoukpe <romaricaholoukpe19@gmail.com>
2025-12-04 02:34:57 +0900

    feat: add lab5 task 1

---
+++ lab5-1-table.sql
@@ -0,0 +1,23 @@
+-- Создание таблицы institutes
+CREATE TABLE institutes (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(1024) NOT NULL
+);
+
+-- Создание таблицы groups
+CREATE TABLE groups (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(2048) NOT NULL,
+    short_name VARCHAR(64) NOT NULL UNIQUE,
+    year INTEGER NOT NULL,
+    institute_id INTEGER NOT NULL
+);
+
+-- Создание таблицы students
+CREATE TABLE students (
+    id SERIAL PRIMARY KEY,
+    surname VARCHAR(512) NOT NULL,
+    name VARCHAR(512) NOT NULL,
+    father_name VARCHAR(512),
+    group_id INTEGER NOT NULL
+);
